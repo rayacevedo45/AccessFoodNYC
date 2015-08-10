3155cc0e0d8f4d3ed171e63da982f071115b41b1
rayacevdo45 <rayacevedo45@gmail.com>
2015-08-10 15:11:21 -0400

    map class

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/MainActivity.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/MainActivity.java
@@ -1,6 +1,8 @@
 package rayacevedo45.c4q.nyc.accessfoodnyc;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -22,6 +24,25 @@ public class MainActivity extends AppCompatActivity {
         accessTV.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.lr));
         foodTV.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.rl));
         NYCtv.startAnimation(AnimationUtils.loadAnimation(MainActivity.this, R.anim.lr));
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(MainActivity.this, Map.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, 2900);
     }
 
     @Override
