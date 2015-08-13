7af37c256722920fb61b1b144faf70cce9bc767b
Luke Desktop <lukesterlee@gmail.com>
2015-08-13 02:52:17 -0400

    google sign in log in flow

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/SplashActivity.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/SplashActivity.java
@@ -1,6 +1,7 @@
 package rayacevedo45.c4q.nyc.accessfoodnyc;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -10,20 +11,25 @@ import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
-    TextView accessTV,foodTV,NYCtv;
+
+    private TextView mTextViewAccess;
+    private TextView mTextViewFood;
+    private TextView mTextViewNYC;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        accessTV = (TextView) findViewById(R.id.accessID);
-        foodTV = (TextView) findViewById(R.id.foodID);
-        NYCtv = (TextView) findViewById(R.id.NYCID);
+        mTextViewAccess = (TextView) findViewById(R.id.accessID);
+        mTextViewFood = (TextView) findViewById(R.id.foodID);
+        mTextViewNYC = (TextView) findViewById(R.id.NYCID);
 
-        accessTV.startAnimation(AnimationUtils.loadAnimation(SplashActivity.this, R.anim.lr));
-        foodTV.startAnimation(AnimationUtils.loadAnimation(SplashActivity.this, R.anim.rl));
-        NYCtv.startAnimation(AnimationUtils.loadAnimation(SplashActivity.this, R.anim.lr));
+        mTextViewAccess.startAnimation(AnimationUtils.loadAnimation(SplashActivity.this, R.anim.lr));
+        mTextViewFood.startAnimation(AnimationUtils.loadAnimation(SplashActivity.this, R.anim.rl));
+        mTextViewNYC.startAnimation(AnimationUtils.loadAnimation(SplashActivity.this, R.anim.lr));
 
         new Handler().postDelayed(new Runnable() {
 
@@ -34,6 +40,8 @@ public class SplashActivity extends AppCompatActivity {
 
             @Override
             public void run() {
+
+
                 // This method will be executed once the timer is over
                 // Start your app main activity
                 Intent i = new Intent(SplashActivity.this, MapsActivity.class);
@@ -45,25 +53,5 @@ public class SplashActivity extends AppCompatActivity {
         }, 2900);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/accounts/LoginActivity.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/accounts/LoginActivity.java
@@ -2,8 +2,10 @@ package rayacevedo45.c4q.nyc.accessfoodnyc.accounts;
 
 import android.app.AlertDialog;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,6 +15,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.plus.Plus;
@@ -24,22 +27,32 @@ import rayacevedo45.c4q.nyc.accessfoodnyc.MapsActivity;
 import rayacevedo45.c4q.nyc.accessfoodnyc.R;
 import rayacevedo45.c4q.nyc.accessfoodnyc.ParseApplication;
 
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
+    private static final String TAG = "LoginActivity";
+
+    private static final int RC_SIGN_IN = 0;
+
+    // Is there a ConnectionResult resolution in progress?
+    private boolean mIsResolving = false;
+
+    // Should we automatically resolve ConnectionResults when possible?
+    private boolean mShouldResolve = false;
 
     private GoogleApiClient mGoogleApiClient;
 
     protected EditText usernameEditText;
     protected EditText passwordEditText;
     protected Button loginButton;
+    private SignInButton mSignInButton;
 
     protected TextView signUpTextView;
 
     protected ParseApplication app;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
         // Build GoogleApiClient with access to basic profile
         mGoogleApiClient = new GoogleApiClient.Builder(this)
@@ -60,16 +73,10 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 //
 //        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
-
-
-        setContentView(R.layout.activity_login);
-
         app = new ParseApplication();
 
-        signUpTextView = (TextView)findViewById(R.id.signUpText);
-        usernameEditText = (EditText)findViewById(R.id.usernameField);
-        passwordEditText = (EditText)findViewById(R.id.passwordField);
-        loginButton = (Button)findViewById(R.id.loginButton);
+        initializeViews();
+
 
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -126,6 +133,22 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         });
     }
 
+    private void initializeViews() {
+        signUpTextView = (TextView)findViewById(R.id.signUpText);
+        usernameEditText = (EditText)findViewById(R.id.usernameField);
+        passwordEditText = (EditText)findViewById(R.id.passwordField);
+        loginButton = (Button)findViewById(R.id.loginButton);
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+    }
+
+    private void setUpListeners(boolean isResumed) {
+        if (isResumed) {
+            mSignInButton.setOnClickListener(this);
+        } else {
+            mSignInButton.setOnClickListener(null);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -147,7 +170,14 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     @Override
     public void onConnected(Bundle bundle) {
+        // onConnected indicates that an account was selected on the device, that the selected
+        // account has granted any requested permissions to our app and that we were able to
+        // establish a service connection to Google Play services.
+        Log.d(TAG, "onConnected:" + bundle);
+        mShouldResolve = false;
 
+        // Show the signed-in UI
+        //showSignedInUI();
     }
 
     @Override
@@ -157,7 +187,37 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (!mIsResolving && mShouldResolve) {
+            if (connectionResult.hasResolution()) {
+                try {
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mIsResolving = true;
+                } catch (IntentSender.SendIntentException e) {
+                    Log.e(TAG, "Could not resolve ConnectionResult.", e);
+                    mIsResolving = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                // Could not resolve the connection result, show the user an
+                // error dialog.
+                //showErrorDialog(connectionResult);
+            }
+        } else {
+            // Show the signed-out UI
+            //showSignedOutUI();
+        }
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpListeners(true);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        setUpListeners(false);
     }
 
     @Override
@@ -170,5 +230,38 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     protected void onStop() {
         super.onStop();
         mGoogleApiClient.disconnect();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                onSignInClicked();
+                break;
+        }
+    }
+
+    private void onSignInClicked() {
+        // User clicked the sign-in button, so begin the sign-in process and automatically
+        // attempt to resolve any errors that occur.
+        mShouldResolve = true;
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // If the error resolution was not successful we should not resolve further.
+            if (resultCode != RESULT_OK) {
+                mShouldResolve = false;
+            }
+
+            mIsResolving = false;
+            mGoogleApiClient.connect();
+        }
     }
 }
