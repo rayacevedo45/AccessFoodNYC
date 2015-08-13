fc4622be351ef2212735638fc01dbbdb117a344b
Luke Desktop <lukesterlee@gmail.com>
2015-08-13 01:35:17 -0400

    google sign in working on

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/accounts/LoginActivity.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/accounts/LoginActivity.java
@@ -3,7 +3,7 @@ package rayacevedo45.c4q.nyc.accessfoodnyc.accounts;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,6 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.Scopes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.plus.Plus;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
@@ -19,7 +24,10 @@ import rayacevedo45.c4q.nyc.accessfoodnyc.MapsActivity;
 import rayacevedo45.c4q.nyc.accessfoodnyc.R;
 import rayacevedo45.c4q.nyc.accessfoodnyc.ParseApplication;
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+
+    private GoogleApiClient mGoogleApiClient;
 
     protected EditText usernameEditText;
     protected EditText passwordEditText;
@@ -31,6 +39,15 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+
+        // Build GoogleApiClient with access to basic profile
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Plus.API)
+                .addScope(new Scope(Scopes.PROFILE))
+                .build();
 //        Parse.initialize(this);
 //
 //        ParseACL defaultACL = new ParseACL();
@@ -44,7 +61,7 @@ public class LoginActivity extends ActionBarActivity {
 //        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
 
 
-        super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_login);
 
         app = new ParseApplication();
@@ -126,5 +143,32 @@ public class LoginActivity extends ActionBarActivity {
 //            return true;
 //        }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mGoogleApiClient.disconnect();
     }
 }
