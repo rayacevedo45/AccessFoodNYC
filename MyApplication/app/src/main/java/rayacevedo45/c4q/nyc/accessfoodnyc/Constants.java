acc268707a0c5f567d44f9e1eb4a89045df7437a
Luke Desktop <lukesterlee@gmail.com>
2015-08-14 17:25:37 -0400

    kinda working

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/Constants.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final double DEFAULT_LATITUDE = 40.740981;
     public static final double DEFAULT_LONGITUDE = -73.899102;
     public static final String EXTRA_KEY_VENDOR_OBJECT_ID = "objectId";
+
 }
