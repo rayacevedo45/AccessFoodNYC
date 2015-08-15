a1e5f0ffc65bb205167bd63e56a941123b66707a
hoshikoo <hoshikooki@gmail.com>
2015-08-15 19:10:25 -0400

    Merge Hoshiko_data2 branch

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/DetailsFragment.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/DetailsFragment.java
@@ -1,22 +1,73 @@
 package rayacevedo45.c4q.nyc.accessfoodnyc;
 
+
+
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import rayacevedo45.c4q.nyc.accessfoodnyc.R;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.squareup.picasso.Picasso;
+
+import java.net.URL;
 
 public class DetailsFragment extends Fragment {
+    ParseObject vendor = new ParseObject("Vendor");
+    String category;
+    static String vendorName;
+    URL vendorPicUrl;
+    String vendorPicUrlStr;
+    TextView categoryText;
+    TextView vendorNameText;
+    ImageView vendorPicImage;
 
-
-    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View result = inflater.inflate(R.layout.fragment_details, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        return result;
+        View rootView = inflater.inflate(R.layout.fragment_details, container, false);
+        vendorNameText = (TextView)rootView.findViewById(R.id.vendor_name);
+        categoryText = (TextView)rootView.findViewById(R.id.category);
+        vendorPicImage = (ImageView)rootView.findViewById(R.id.vendor_pic);
+        return rootView;
     }
+
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getLatestPosts();
+
+    }
+
+    protected void getLatestPosts(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Vendor");
+        query.getInBackground("i6TquTtMsJ", new GetCallback<ParseObject>() {
+            public void done(ParseObject vendor, ParseException e) {
+                if (e == null) {
+                    vendorName = vendor.getString("vendor_name");
+                    vendorNameText.setText("Name: "+vendorName);
+                    category = vendor.getString("category");
+                    categoryText.setText("Category: "+category);
+                    vendorPicUrlStr = vendor.getString("picture_url");
+                    Picasso.with(getActivity()).load(vendorPicUrlStr).centerCrop().resize(1100, 700).into(vendorPicImage);
+
+                } else {
+                    // something went wrong
+                }
+            }
+        });
+
+    }
+
 }
--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/MenuFragment.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/MenuFragment.java
@@ -1,21 +1,57 @@
 package rayacevedo45.c4q.nyc.accessfoodnyc;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-import rayacevedo45.c4q.nyc.accessfoodnyc.R;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.squareup.picasso.Picasso;
 
 public class MenuFragment extends Fragment {
 
-    @Nullable
+    ParseObject vendor = new ParseObject("Vendor");
+    ImageView menuImage;
+    String menuPicUrlStr;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View result = inflater.inflate(R.layout.fragment_menu, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
 
-        return result;
+        View rootView = inflater.inflate(R.layout.fragment_menu, container, false);
+        menuImage = (ImageView)rootView.findViewById(R.id.menu_pic);
+        return rootView;
+
+
+    }
+
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        getLatestPosts();
+
+    }
+
+    protected void getLatestPosts(){
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Vendor");
+        query.getInBackground("i6TquTtMsJ", new GetCallback<ParseObject>() {
+            public void done(ParseObject vendor, ParseException e) {
+                if (e == null) {
+                    menuPicUrlStr = vendor.getString("menu_url");
+                    Picasso.with(getActivity()).load(menuPicUrlStr).into(menuImage);
+
+                } else {
+                    // something went wrong
+                }
+            }
+        });
+
     }
 }
