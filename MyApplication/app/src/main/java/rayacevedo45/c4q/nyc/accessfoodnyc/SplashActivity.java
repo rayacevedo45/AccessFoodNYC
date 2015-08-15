8ebeb327a05acf35141d5778a402fb6d364e8ce3
annalinewyork <annalinewyork@gmail.com>
2015-08-15 16:33:21 -0400

    navigate to Login from Splash (instead of Maps)

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/SplashActivity.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/SplashActivity.java
@@ -42,7 +42,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashActivity.this, MapsActivity.class);
+                Intent i = new Intent(SplashActivity.this, LoginActivity.class);
                 startActivity(i);
 
                 // close this activity
