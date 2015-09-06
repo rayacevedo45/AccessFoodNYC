d0e1391b3661fbd880f737f421262afa564b49a9
hoshikoo <hoshikooki@gmail.com>
2015-09-05 21:04:39 -0400

    fix twitter text

--- MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/ReviewsFragment.java
+++ MyApplication/app/src/main/java/rayacevedo45/c4q/nyc/accessfoodnyc/ReviewsFragment.java
@@ -209,7 +209,7 @@ public class ReviewsFragment extends Fragment implements View.OnClickListener {
                             }
                         });
 
-                    } 
+                    }
                 }
             });
         }
