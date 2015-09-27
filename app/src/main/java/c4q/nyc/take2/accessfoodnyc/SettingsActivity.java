a1583346761046c3601208afaa3789c3901e75ed
MadelynTav <Madelyntav@gmail.com>
2015-09-27 19:29:59 -0400

    added links for settings activity to collapsible toolbars

--- app/src/main/java/c4q/nyc/take2/accessfoodnyc/SettingsActivity.java
+++ app/src/main/java/c4q/nyc/take2/accessfoodnyc/SettingsActivity.java
@@ -10,11 +10,4 @@ public class SettingsActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.settings);
     }
-
-    /*TODO to use the settingsActivity to check if you can send notifications check the status of boolean
-    SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-    Boolean notificationsPref=sharedPref.getBoolean("notifications_switch",true);
-
-    TODO: Add button that will link to settings activity?
-     */
 }
